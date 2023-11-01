6c9751a1dde73a6703b32f611ee4342f477a68c9
Diancent <artem.korol.jet@gmail.com>
2023-11-02 00:20:11 +0200

    Rebase branch hierarchy

---
+++ Backend/Controllers/AuthController.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Models;
+using Backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private static User user = new User();
+        private readonly IConfiguration _configuration;
+        private readonly IUserService _userService;
+
+        public AuthController(IConfiguration configuration, IUserService userService)
+        {
+            _configuration = configuration;
+            _userService = userService;
+        }
+
+        [HttpGet, Authorize]
+        public ActionResult<string> GetMyName()
+        {
+            return Ok(_userService.GetMyName());
+        }
+        
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            // Перевірка, чи користувач з такою електронною адресою вже існує
+            if (user.Email == request.Email)
+            {
+                return BadRequest("User with the same email already exists.");
+            }
+
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            user.Username = request.Username;
+            user.Email = request.Email;
+            user.PasswordHash = passwordHash;
+
+            // Логіка для відправлення підтверджувального листа
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(UserDto request)
+        {
+            if (user.Email != request.Email)
+            {
+                return BadRequest("User not found.");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                return BadRequest("Wrong password.");
+            }
+
+            string token = await CreateToken(user);
+
+            var refreshToken = await GenerateRefreshToken(); 
+            await SetRefreshToken(refreshToken);
+
+            return Ok(token);
+        }
+        
+        [HttpPost("make-admin")]
+        [Authorize(Roles = "Admin")]
+        public ActionResult MakeAdmin(UserDto request)
+        {
+            if (user.Email == request.Email && BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+            {
+                var identity = User.Identity as ClaimsIdentity;
+                identity?.AddClaim(new Claim(ClaimTypes.Role, "Admin"));
+
+                return Ok("User is now an Admin.");
+            }
+
+            return BadRequest("User not found or invalid password.");
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<ActionResult<string>> RefreshToken()
+        {
+            var refreshToken = Request.Cookies["refreshToken"];
+
+            if (!user.RefreshToken.Equals(refreshToken))
+            {
+                return Unauthorized("Invalid Refresh Token");
+            }
+            else if (user.TokenExpires < DateTime.Now)
+            {
+                return Unauthorized("Token expired.");
+            }
+
+            string token = await CreateToken(user);
+            var newRefreshToken = await GenerateRefreshToken();
+            await SetRefreshToken(newRefreshToken);
+
+            return Ok(token);
+        }
+
+        private async Task<RefreshToken> GenerateRefreshToken()
+        {
+            var refreshToken = new RefreshToken
+            {
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                Expires = DateTime.Now.AddDays(7)
+            };
+
+            return refreshToken;
+        }
+
+        private async Task SetRefreshToken(RefreshToken newRefreshToken)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = newRefreshToken.Expires,
+            };
+            Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieOptions);
+
+            user.RefreshToken = newRefreshToken.Token;
+            user.TokenCreated = newRefreshToken.Created;
+            user.TokenExpires = newRefreshToken.Expires;
+        }
+
+
+        private async Task<string> CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "User")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
