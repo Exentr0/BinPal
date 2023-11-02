de784e08b6f5cb6a1111b8e9cca020658fb1a034
evkalipt <oleksiitatar@gmail.com>
2023-11-02 21:17:14 +0200

    cleaned the extra files. created comnonent auth

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import {UserTest} from "./modules/user-test";
-import {UserTestService} from "./services/user-test.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Frontend';
-  userTests : UserTest[] = [];
-  constructor(private userTestService: UserTestService) { }
 
-  ngOnInit(): void{
-    this.userTestService
-        .getUserTests()
-        .subscribe((result: UserTest[]) => (this.userTests = result));
-  }
 }
+
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from './app.component';
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import {HttpClientModule} from "@angular/common/http";
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
