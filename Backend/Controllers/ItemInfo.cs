f7cf0650ff7999fa968378df949b95ffd695078e
chukivskiy <ivan.chukivskiy@gmail.com>
2023-12-16 22:43:31 +0200

    more info and new database

--- Backend/Controllers/ItemInfo.cs
+++ Backend/Controllers/ItemInfo.cs
@@ -2,17 +2,19 @@ using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using Backend.Data;
 using Backend.Models;
-
+using Backend.Services;
 namespace Backend.Controllers;
+using Backend.Services.Storage;
 [Route("api/items")]
 [ApiController]
 public class ItemController : ControllerBase
 {
     private readonly DataContext _context;
-
-    public ItemController(DataContext context)
+    private readonly ItemPicturesBlobService _itemPicturesBlobService;
+    public ItemController(DataContext context, ItemPicturesBlobService itemPicturesBlobService)
     {
         _context = context;
+        _itemPicturesBlobService = itemPicturesBlobService;
     }
 
     [HttpGet("{id}")]
@@ -25,16 +27,18 @@ public class ItemController : ControllerBase
         }
 
         var owner = _context.Users.FirstOrDefault(u => u.Id == item.PublisherId);
-
+        var itemPictureUrls = _itemPicturesBlobService.GetItemPictureUrlsAsync(id);
         var result = new
         {
+            Id = id,
             Name = item.Name,
             Price = item.Price,
             Owner = owner.Username,
             Rating = item.Rating,
             Overview = item.Description,
             PublisherInfo = item.PublisherInfo,
-            License = item.License
+            License = item.License, 
+            Pictures = itemPictureUrls
         };
 
         return new JsonResult(result);
