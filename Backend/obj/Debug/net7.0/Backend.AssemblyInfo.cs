2363fa2e47471c8cfa2beebe59ee02ae6b39fb55
Puhachevskyi Dmytro <106034451+Dimus0@users.noreply.github.com>
2023-12-17 15:35:59 +0200

    Сортування

--- Backend/obj/Debug/net7.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net7.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2caab7d3faad90f69db3fa640ba5bc26a8355cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
