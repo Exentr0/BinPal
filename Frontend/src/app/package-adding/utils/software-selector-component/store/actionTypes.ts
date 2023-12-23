a9d9c270bff11ddaa81fce17b985527fe730a2d9
Exentr0 <viktorhotsiy@gmail.com>
2023-12-23 12:03:44 +0200

    Oleksiy dayn ne vidaliv, teper chischu za nim

--- Frontend/src/app/package-adding/utils/software-selector-component/store/actionTypes.ts
+++
@@ -1,5 +0,0 @@
-export enum ActionTypes {
-    GET_SOFTWARE = '[Software] Get software',
-    GET_SOFTWARE_SUCCESS = '[Software] Get software success',
-    GET_SOFTWARE_FAILURE = '[Software] Get software failure'
-}
--- Frontend/src/app/package-adding/utils/software-selector-component/store/types/softwareState.interface.ts
+++
@@ -1,7 +0,0 @@
-import {SoftwareInterface} from "../../../../../shared/types/software.interface";
-
-export interface SoftwareStateInterface{
-    isLoading: boolean
-    error: string | null
-    data: SoftwareInterface[] | null
-}
